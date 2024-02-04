2fa00796ba23bb2034c2a61e2f8e0c2f5d0a0854
杨亚杰 <39705429+JieGenius@users.noreply.github.com>
2024-02-04 16:02:13 +0800

    指定启动ip

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import os
 
 if __name__ == '__main__':
-    os.system('streamlit run web_demo.py --server.port 7860 --server.enableStaticServing True')
+    os.system('streamlit run web_demo.py --server.address=0.0.0.0 --server.port 7860 --server.enableStaticServing True')
