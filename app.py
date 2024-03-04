97792b3ba9cf8ce0f8b39446e7527d11c12b7671
杨亚杰 <39705429+JieGenius@users.noreply.github.com>
2024-03-04 14:29:56 +0800

    修改requirements.txt

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ import time
 
 import torch
 
-# os.system("pip install torch==2.0.0 torchvision==0.15.1 "
-#           "torchaudio==2.0.1 "
-#           "--index-url https://download.pytorch.org/whl/cu118")
+os.system("pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 "
+          "--index-url https://download.pytorch.org/whl/cu118")
 
 MODEL_DIR = 'models/'
 MODEL_REPOSITORY_OPENXLAB = 'flyer/OculiChatDA'
